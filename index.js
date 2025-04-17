46c4cdac93b454342d77f7efac93a166c61b638b
Harshit Pandey <harshitpandey@jklu.edu.in>
2025-04-17 10:40:03 +0530

    api change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const serverless = require("serverless-http");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("../config/db");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config();
@@ -28,12 +28,12 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 // Routes
-const userRoutes = require("../routes/userRoutes");
-const petRoutes = require("../routes/petRoutes");
-const appointmentRoutes = require("../routes/appointmentRoutes");
-const messageRoutes = require("../routes/messageRoutes");
-const healthRecordRoutes = require("../routes/healthRecordRoutes");
-const dashboardRoutes = require("../routes/dashboardRoutes");
+const userRoutes = require("./routes/userRoutes");
+const petRoutes = require("./routes/petRoutes");
+const appointmentRoutes = require("./routes/appointmentRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const healthRecordRoutes = require("./routes/healthRecordRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 app.use("/api/users", userRoutes);
 app.use("/api/pets", petRoutes);
